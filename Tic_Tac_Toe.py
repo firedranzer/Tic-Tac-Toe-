050e2da7b9ae6c683a3c2d4863e3081f3ce061c2
rpz-harode <rupeshharode@gmail.com>
2017-06-06 16:42:13 +0530

    Python Project

---
+++ Tic_Tac_Toe.py
@@ -0,0 +1,21 @@
+def board_input(self, parameter_list):
+    index[]=input('What is your move(1-9)')
+    board
+
+def board(self, parameter_list):
+    print '  |  |  '
+    print ' index[7] | index[8] | index[9] '
+    print '  |  |  '
+    print '--------'
+    print '  |  |  '
+    print ' index[7] | index[8] | index[9] '
+    print '  |  |  '
+    print '--------'
+    print '  |  |  '
+    print ' index[7] | index[8] | index[9] '
+    print '  |  |  '
+
+def board_check(self, parameter_list):
+    
+    
+def board_input
