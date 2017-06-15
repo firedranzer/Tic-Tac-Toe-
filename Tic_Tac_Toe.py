c53b1a2a3738be5af6a06da91ba38c4f6d9bf713
Rupesh Harode <rupesh@Rupeshs-MacBook-Air.local>
2017-06-16 00:31:05 +0530

    Added Functions

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,5 +1,5 @@
-import __future__ from print_function
-from IPython.display import clear_output
+from __future__ import print_function
+#from IPython.display import clear_output
 import random
 
 #Printing Board
@@ -33,27 +33,46 @@ def player_input():
 def place_marker(board,marker,position):
     board[position] = marker
     
-#function to check if the position is empty   
-def check_board(index[i]):
-    return (index[i]=='')
+#function to check if the position on the board is empty
+def check_board(board,position):
+    return board[position] == ' '
 
 #Function to check if any player has won
 def win_check(board,marker):
-    return (board[1]==marker and board[2]==marker and board[3]==marker) or 
+    return ((board[1]==marker and board[2]==marker and board[3]==marker) or
     (board[4]==marker and board[5]==marker and board[6]==marker) or
     (board[7]==marker and board[8]==marker and board[9]==marker) or
     (board[1]==marker and board[4]==marker and board[7]==marker) or
     (board[2]==marker and board[5]==marker and board[8]==marker) or
     (board[3]==marker and board[6]==marker and board[9]==marker) or
     (board[1]==marker and board[5]==marker and board[9]==marker) or
-    (board[3]==marker and board[5]==marker and board[7]==marker) 
+    (board[3]==marker and board[5]==marker and board[7]==marker))
 
-#Function to choose player at random
+#Function to choose first player at random to start the game
 def choose_first(parameter_list):
     if random.randint(0,1)==0:
         return 'Player 1:'
-    else
+    else:
         return 'Player 2:'
+
+#Function to check for the board to be full
+def full_board_check(board):
+    for i in range(0,10):
+        if check_board(board,i)
+            return False
+        return True
+
+#Function to decide next player's move
+def player_choice(board, position=None):
+    position = ' '
+    while position not in (1 2 3 4 5 6 7 8 9).split() or not check_board(board,int(position)):
+        position = raw_input("Choose your next position (1-9) :")
+
+    return int(position)
+
+#Function to devide if the player wants to play again
+def replay():
+    return raw_input("Do you want to play again(yes or no)").lower().startswith('y')
 
 
 
